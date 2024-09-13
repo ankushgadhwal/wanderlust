bfd9eaf45efdca0455e0e145ecd09d011b60a39c
ankushgadhwal <ankushgadhwal999@gmail.com>
2024-09-13 07:28:27 +0530

    getting ready for hosting

--- controllers/listings.js
+++ controllers/listings.js
@@ -44,10 +44,8 @@ module.exports.createListing = async (req, res, next) => {
   const newListing = new Listing(req.body.listing);
   newListing.owner = req.user._id;
   newListing.image = { url, filename };
-
   newListing.geometry = response.body.features[0].geometry;
   let savedListing = await newListing.save();
-  console.log(savedListing);  
   req.flash("success", "New Listing Created!");
   res.redirect("/listings");
 }
--- controllers/users.js
+++ controllers/users.js
@@ -9,7 +9,6 @@ module.exports.signup = async (req, res, next) => {
     let { username, email, password } = req.body;
     const newUser = new User({ username, email });
     const registerUser = await User.register(newUser, password);
-    console.log(registerUser);
     req.login(registerUser, (err) => {
       if (err) {
         return next(err);
