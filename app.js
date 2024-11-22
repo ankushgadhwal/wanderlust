2801c25ee7dda4b55ad8f873e1e3ae3498442d44
ankush <ankushgadhwal999@gmail.com>
2024-11-22 13:19:36 +0530

    minor changes

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV != "production") {
-  require('dotenv').config();
+  require("dotenv").config();
 }
 
 const express = require("express");
@@ -10,7 +10,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError");
 const session = require("express-session");
-const MongoStore = require('connect-mongo');
+const MongoStore = require("connect-mongo");
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
@@ -21,7 +21,7 @@ const reviewRouter = require("./routes/review");
 const userRouter = require("./routes/user");
 
 // const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
-const dbUrl = process.env.ATLASDB_URL
+const dbUrl = process.env.ATLASDB_URL;
 
 main()
   .then(() => {
@@ -33,37 +33,37 @@ main()
 
 async function main() {
   await mongoose.connect(dbUrl);
-};
+}
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-app.engine('ejs', ejsMate);
+app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
 const store = MongoStore.create({
   mongoUrl: dbUrl,
   crypto: {
-    secret: "mysuperscretcode",
+    secret: process.env.SECRET,
   },
   touchAfter: 24 * 3600,
 });
 
 store.on("error", () => {
-  console.log("ERROR in MONGO SESSION STORE", err);  
+  console.log("ERROR in MONGO SESSION STORE", err);
 });
 
 const sessionOption = {
   store,
-  secret: "mysuperscretcode",
+  secret: process.env.SECRET,
   resave: false,
   saveUninitialized: true,
   cookie: {
     expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
     maxAge: 1000 * 60 * 60 * 24 * 7,
     httpOnly: true,
-  }
+  },
 };
 
 // app.get("/", (req, res) => {
@@ -91,15 +91,15 @@ app.use("/listings/:id/reviews", reviewRouter);
 app.use("/", userRouter);
 
 app.all("*", (req, res, next) => {
-  next(new ExpressError(404, "Page Not Found"))
+  next(new ExpressError(404, "Page Not Found"));
 });
 
 app.use((err, req, res, next) => {
   let { statusCode = 500, message = "Something went wrong" } = err;
-  res.status(statusCode).render("error.ejs", { message })
+  res.status(statusCode).render("error.ejs", { message });
   // res.status(statusCode).send(message);
 });
 
 app.listen("8080", () => {
   console.log("Server is listening to port 8080");
-})
+});
